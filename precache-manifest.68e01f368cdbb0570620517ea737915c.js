9451ffbc13e81cf496e80b1901103fe7f3d6d6c7
Suraj <surajthakur10074@gmail.com>
2020-08-04 22:35:06 +0530

    Updates

---
+++ precache-manifest.68e01f368cdbb0570620517ea737915c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ca22eecbd4c0377873f66aa23028d0d",
+    "url": "/photowall-app/index.html"
+  },
+  {
+    "revision": "fc6ed6d5dfe4ca9ac7f9",
+    "url": "/photowall-app/static/css/main.3f11256d.chunk.css"
+  },
+  {
+    "revision": "4d3ad7e839db698d43b3",
+    "url": "/photowall-app/static/js/2.73d6e460.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/photowall-app/static/js/2.73d6e460.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc6ed6d5dfe4ca9ac7f9",
+    "url": "/photowall-app/static/js/main.2dfbe9fc.chunk.js"
+  },
+  {
+    "revision": "00f63496f2d7d359e462",
+    "url": "/photowall-app/static/js/runtime-main.bfac3b52.js"
+  }
+]);
